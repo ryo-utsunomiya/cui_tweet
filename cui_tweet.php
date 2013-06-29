d6c182d4a9ccf04a19d06eee8bc37f573938bb56
Ryo Utsunomiya <millionsnowliving@gmail.com>
2013-06-29 17:35:11 +0900

    引数で投稿

--- cui_tweet.php
+++ cui_tweet.php
@@ -3,7 +3,6 @@
 
 /**
 * cui_tweet
-* ターミナルでこっそりツイートするスクリプト
 * 
 * @author Ryo Utsunomiya(@ryo511)
 * @see codebird-php(https://github.com/mynetx/codebird-php)
@@ -33,19 +32,9 @@ $cb = new \Codebird\Codebird(); // Codebirdインスタンスを作成
 $cb->setConsumerKey($consumer_key, $consumer_secret);
 $cb->setToken($access_token, $access_token_secret);
 
-// ユーザ名をローカルファイルから読み取り
-$userNameDir = __DIR__ . '/username.txt';
-if (is_readable($userNameDir)) {
-    $useName = file_get_contents($userNameDir);
-} else { // ユーザ名設定ファイルの読み込みに失敗した場合はファイルを新規作成
-    file_put_contents($userNameDir, '');
-}
-$userName = trim($userName);
-echo $userName . '>'; // プロンプト表示
-
 // ユーザ入力に応じてツイートを行う
-$input = fopen('php://stdin', 'r'); // 標準入力ストリームを取得
-while ($in = fgets($input)) { // ユーザ入力を受け付け
+if (isset($argv[1])) {
+    $in = $argv[1];
     $in = trim($in); // 空白除去
     if ('exit' === $in) { // 'exit'が入力されたら終了
         exit;
@@ -55,9 +44,36 @@ while ($in = fgets($input)) { // ユーザ入力を受け付け
     } else { // 入力に問題ない場合はツイートを行う
         $reply = $cb->statuses_update('status=' . $in);
         errorCheck($reply); // twitterから返ってきた値にエラーがないかチェック
-        nameCheck($reply); // ユーザ名に変更がないかチェック
     }
+    exit;
+
+} else {
+
+    // ユーザ名をローカルファイルから読み取り
+    $userNameDir = __DIR__ . '/username.txt';
+    if (is_readable($userNameDir)) {
+        $userName = file_get_contents($userNameDir);
+    } else { // ユーザ名設定ファイルの読み込みに失敗した場合はファイルを新規作成
+        file_put_contents($userNameDir, '');
+    }
+    $userName = trim($userName);
     echo $userName, '>';
+
+    $input = fopen('php://stdin', 'r'); // 標準入力ストリームを取得
+    while ($in = fgets($input)) { // ユーザ入力を受け付け
+        $in = trim($in); // 空白除去
+        if ('exit' === $in) { // 'exit'が入力されたら終了
+            exit;
+        }
+        if ($warning = lengthCheck($in)) { // 文字数チェック
+            echo $warning, PHP_EOL; // 警告が返ってきた場合は表示
+        } else { // 入力に問題ない場合はツイートを行う
+            $reply = $cb->statuses_update('status=' . $in);
+            errorCheck($reply); // twitterから返ってきた値にエラーがないかチェック
+            nameCheck($reply); // ユーザ名に変更がないかチェック
+        }
+        echo $userName, '>';
+    }
 }
 
 // ユーザ名に変更があったらローカルファイルを書き換え
@@ -80,7 +96,7 @@ function errorCheck($reply) {
 
 // 文字数が0又は140文字を超えている場合に警告
 function lengthCheck($str) {
-    $length = strlen($str);
+    $length = mb_strlen($str, 'UTF-8');
     if (0 === $length) {
         return 'ポストする文字列がありません';
     } elseif (140 < $length) {
